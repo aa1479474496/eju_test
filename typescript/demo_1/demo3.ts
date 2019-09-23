fe79733e253bf4a94814d451d9dab5d991ca6e93
shaonan.zha <1479474496@qq.com>
2019-09-23 17:24:03 +0800

    typescript demo

---
+++ typescript/demo_1/demo3.ts
@@ -0,0 +1,10 @@
+interface searchFunc {
+    (source: string, substring: string): boolean;
+}
+
+let mySearch: searchFunc;
+mySearch = function(src: string, sub: string): boolean {
+    let result = src.search(sub);
+    return result > -1;
+}
+console.log(mySearch('abcdefg', 'def'));
