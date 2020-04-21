9221ee95d7e4c414be4b3b09f685463c07cb8b09
zhashaonan <1479474496@qq.com>
2020-04-21 23:31:52 +0800

    add antd

--- reactDemo/hook-ts-demo/src/App.tsx
+++ reactDemo/hook-ts-demo/src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Link, Route } from 'react-router-dom';
 
 import InitHooks from './initHooks/InitHooks';
 import HooksSimple from './hooksSimple/HooksSimple';
+import FakeHooks from './fakeHooks/Index';
 import './App.css';
 
 // const user = <User name='vortesnail' age={25}>I am children text!</User>
@@ -13,13 +14,12 @@ function App() {
             <div>
                 <Link className="link" to='/'>initHooks</Link>
                 <Link className="link" to='/simple'>hooksSimple</Link>
+                <Link className="link" to='/fakehooks'>fakehooks</Link>
                 <div style={{ padding: '20px', border: '1px solid green', margin: '20px' }}>
                     <Route path="/" exact component={InitHooks}></Route>
                     <Route path="/simple" component={HooksSimple}></Route>
+                    <Route path="/fakehooks" component={FakeHooks}></Route>
                 </div>
-
-
-
             </div>
         </Router>
     );
---
+++ reactDemo/hook-ts-demo/src/fakeHooks/Index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Button from 'antd/es/button';
+import { Button } from 'antd';
+const FakeHooks = () => {
+    return (
+        <div>
+            fakdeHooks
+
+            <Button type="primary">Primary</Button>
+        </div>
+    )
+}
+
+export default FakeHooks;
