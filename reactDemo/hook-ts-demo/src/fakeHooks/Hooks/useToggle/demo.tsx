1e1c01b55c457aa0e9e5feffbb1a0f22c76f2aed
zhashaonan <1479474496@qq.com>
2020-05-10 22:31:26 +0800

    test useToggle

--- reactDemo/hook-ts-demo/src/fakeHooks/Hooks/useToggle/demo.tsx
+++ reactDemo/hook-ts-demo/src/fakeHooks/Hooks/useToggle/demo.tsx
@@ -3,17 +3,20 @@ import { Button, Switch } from 'antd';
 import useToggle from './useToggle';
 
 const ToggleDemo:React.FC = () => {
-    const { state, toggle } = useToggle();
+    const { state, toggle } = useToggle('Hello', 'World');
     return  (
         <>
             <div>{state}</div>
             <p>
                 Effects:
-                <Switch checked={state} />
+                {/* <Switch checked={state} /> */}
             </p>
             <p>
                 <Button type="default" onClick={() => toggle()}>Toggle</Button>
             </p>
+            <Button type="danger" onClick={() => toggle('Hello')} style={{ margin: '0 16px' }}>
+                Toggle Hello
+            </Button>
         </>
     )
 }
