4eea84d4325d65b3a5d0669e50b2fcd180e0f79d
zhashaonan <1479474496@qq.com>
2020-09-07 10:46:52 +0800

    handsontable 增加中文支持

--- vue_cli3/src/views/HandsonTable/test1.vue
+++ vue_cli3/src/views/HandsonTable/test1.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
     <p>handsontable - test1</p>
+    <hot-table :settings="hotSettings" :language="language"></hot-table>
   </div>
 </template>
+
+<script>
+import 'handsontable/languages/zh-CN';
+import "handsontable/dist/handsontable.full.css";
+import { HotTable } from "@handsontable/vue";
+import Handsontable from 'handsontable';
+export default {
+  components: {
+    HotTable
+  },
+  data() {
+    return {
+      hotSettings: {
+        data: Handsontable.helper.createSpreadsheetData(5, 10),
+        colHeaders: true,
+        rowHeaders: true,
+        contextMenu: true
+      },
+      language: "zh-CN"
+    };
+  },
+  created() {
+    // let _data = Handsontable.helper.createSpreadsheetData(5, 10);
+    // console.log('------', _data);
+  }
+  
+};
+</script>
+
+<style lang="scss">
+#hot-display-license-info {
+  display: none;
+}
+</style>
