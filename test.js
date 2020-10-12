328744f4f418c0d48ef82161b51fcfb15cc66cc2
zhashaonan <1479474496@qq.com>
2020-10-12 10:56:09 +0800

    test.js 增加判断数据类型方法

--- test.js
+++ test.js
@@ -1,85 +1,48 @@
-function isEqual(origin, target) {
-  // 判断数据有没有发生改变
-  if (typeof target !== "object") {
-    //target不是对象/数组
-    return origin === target; //直接返回全等的比较结果
-  }
+// 1.判断对象的数据类型
 
-  if (typeof origin !== "object") {
-    //origin不是对象/数组
-    return false; //直接返回false
-  }
+// const isType = type => target => `[object ${type}]` === Object.prototype.toString.call(target);
+// const isArray = isType('Array');
+// console.log('isArray:::', isArray([123]));
 
-  // console.log("origin", typeof origin, typeof target);
-  for (let key of Object.keys(target)) {
-    //遍历target的所有自身属性的key
-    //   console.log(" Object.keys(target)", Object.keys(target));
-    if (!isEqual(origin[key], target[key])) {
-      //递归比较key对应的value，
-      //value不等，则两对象不等，结束循环，退出函数，返回false
-      //   console.log("key", origin[key], target[key]);
-      return false;
-    }
+function isType(type) {
+  return function (target) {
+    return `[object ${type}]` === Object.prototype.toString.call(target);
   }
-  //遍历结束，所有value都深度比较相等，则两对象相等
-  return true;
 }
 
-let arr1 = [{ name: 1 }];
-let arr2 = [];
-let flag = isEqual(arr1, arr2);
-console.log("flag", flag);
+function isArray(target) {
+  return isType('Array')(target);
+}
 
-// var compareObj = {
-//   // 比较两个对象是否相等
-//   compare: function(oldData, newData) {
-//     // 类型为基本类型时,如果相同,则返回true
-//     if (oldData === newData) return true;
-//     if (
-//       compareObj.isObject(oldData) &&
-//       compareObj.isObject(newData) &&
-//       Object.keys(oldData).length === Object.keys(newData).length
-//     ) {
-//       // 类型为对象并且元素个数相同
-//       // 遍历所有对象中所有属性,判断元素是否相同
-//       for (const key in oldData) {
-//         if (oldData.hasOwnProperty(key)) {
-//           if (!compareObj.compare(oldData[key], newData[key])) {
-//             // 对象中具有不相同属性 返回false
-//             return false;
-//           }
-//         }
-//       }
-//     } else if (
-//       compareObj.isArray(oldData) &&
-//       compareObj.isArray(oldData) &&
-//       oldData.length === newData.length
-//     ) {
-//       // 类型为数组并且数组长度相同
-//       for (let i = 0, length = oldData.length; i < length; i++) {
-//         if (!compareObj.compare(oldData[i], newData[i])) {
-//           // 如果数组元素中具有不相同元素,返回false
-//           return false;
-//         }
-//       }
-//     } else {
-//       // 其它类型,均返回false
-//       return false;
-//     }
-//     // 走到这里,说明数组或者对象中所有元素都相同,返回true
-//     return true;
-//   },
-//   // 判断此类型是否是Array类型
-//   isObject: function(obj) {
-//     return Object.prototype.toString.call(obj) === "[object Object]";
-//   },
-//   // 判断此对象是否是Object类型
-//   isArray: function(arr) {
-//     return Object.prototype.toString.call(arr) === "[object Array]";
-//   }
-// };
+// console.log('isArray:::2', isArray([456]));
 
-// let arr1 = [{ name: 1 }];
-// let arr2 = [];
-// let flag = compareObj.compare(arr1, arr2);
-// console.log("flag", flag);
+//2.循环实现数组 filter 方法
+const selfFilter = function (fn, context) {
+  let arr = Array.prototype.slice.call(this);
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    // console.log('arr[i]', arr[i]);
+    if (!arr.hasOwnProperty(i)) {
+      continue;
+    }
+    console.log('arr[i]', arr[i]);
+
+    fn.call(context, arr[i], i, this) && filteredArr.push(arr[i]);
+  }
+  console.log('----filteredArr', filteredArr);
+  return filteredArr;
+}
+let arr = [1,2,3];
+
+let _arr = selfFilter.call(arr, function(item) {
+  return item > 2;
+});
+console.log('test::', _arr);
+
+// Array.prototype.selfFilter = selfFilter;
+// let arr = [1,2,3];
+// let _arr = arr.selfFilter(function(item) {
+//   console.log('item:', item);
+//   return item > 1;
+// });
+// console.log('_arr:::', _arr);
