eb8cfb329abe7108ee51d298fd0e937475e313f7
zhashaonan <1479474496@qq.com>
2020-11-23 20:41:37 +0800

    增加数据源搜索输入框

--- reactDemo/react_report/src/pages/edit/components/SideBar/PageSource.tsx
+++ reactDemo/react_report/src/pages/edit/components/SideBar/PageSource.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cls from 'classnames';
 
 import { Input } from 'antd';
@@ -10,14 +10,28 @@ import styles from './pageSource.scss';
 
 const PageSource = ({ datas }: { datas: SourceDatasType[] }) => {
   // console.log('----', props);
+  const [inputValue, setInputValue] = useState<string>('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  let _datas = datas.filter(item => {
+    return item.name.indexOf(inputValue) != -1;
+  });
   return (
     <div className={styles.pages_data_container}>
       <div className={styles.bottom_content}>
-        <Input placeholder="请输入内容进行快速搜索" prefix={<SearchOutlined />} />
+        <Input 
+          value={inputValue} 
+          onChange={handleChange} 
+          placeholder="请输入内容进行快速搜索" 
+          allowClear
+          prefix={<SearchOutlined />} 
+        />
       </div>
       <div className={styles.source_list}>
         {
-          datas.map(item => {
+          _datas.map(item => {
             return (
               <div className={styles.item} key={item.name}>
                 <span className={styles.left} title={item.name}>{item.name}</span>
