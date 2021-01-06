d19d95841294e55736dc301e28d16a5c595294fd
zhashaonan <1479474496@qq.com>
2021-01-06 15:12:29 +0800

    test slot

---
+++ vue_cli3/src/views/ElTable/slotTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    slot test
+    <span v-if="$slots.default" class="el-link--inner">
+      <slot></slot>
+    </span>
+    <template v-if="$slots.icon">
+      <slot v-if="$slots.icon" name="icon"></slot>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
