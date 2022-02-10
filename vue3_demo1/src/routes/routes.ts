5b092999d94c3891bb58625c990e87ee298975c3
zhashaonan <1479474496@qq.com>
2022-02-10 15:56:54 +0800

    vue-router init

---
+++ vue3_demo1/src/routes/routes.ts
@@ -0,0 +1,11 @@
+const Home = () => 
+    import(/* webpackChunkName: "Home" */ '../views/home/index.vue')
+
+const routes = [
+    {
+        path: '/home',
+        component: Home
+    }
+]
+
+export default routes;
